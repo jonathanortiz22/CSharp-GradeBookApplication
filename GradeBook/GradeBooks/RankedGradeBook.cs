6da8e31c5cb44354b6bfd28761e115bb508a7279
Jonathan Ortiz <jonathan.ortiz@ust-global.com>
2018-11-07 13:33:57 -0600

    Add Features to Grade Book Application Using c#

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -11,5 +12,28 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var percentStudents = (int)Math.Ceiling(Students.Count*0.2); //will choose the number of students that conform the 20% and round to up
+            //Uses expression lambda (pointer to a function)... First order object student by field AverageGrade, then of the object select the column AverageGrade and it put in a list
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[percentStudents-1] <= averageGrade)
+                return 'A';
+            else if (grades[(percentStudents*2)-1] <= averageGrade)
+                return 'B';
+            else if (grades[(percentStudents*3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(percentStudents*4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
+
     }
 }
